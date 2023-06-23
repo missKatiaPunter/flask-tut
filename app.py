48c0e1e2a0d957c200c9998553ea895632ce6c55
Katia <katia.punter@storyshareplatform.com>
2023-06-23 13:07:24 +0100

    adds frozen

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
+from flask_frozen import Freezer
+import sys
 
 app = Flask(__name__)
+freezer = Freezer(app)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    if len(sys.argv) > 1 and sys.argv[1] == "build":
+        freezer.freeze()
+    else:
+        app.run(debug=True)
+
+
 
